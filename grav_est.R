4364cc8c51a2dcfd742435875c4bb00aee7aaeda
geraldine28 <75378350+geraldine28@users.noreply.github.com>
2022-03-08 13:34:22 +0100

    No changes

--- grav_est.R
+++ grav_est.R
@@ -19,13 +19,14 @@ load( "../Data/WorkData/impuData17.Rdata" )
 
 #### run estimations
 # estimation equation 
-form <- formula(  newarrival ~ index0asylum + log( dist ) + contig + comlang_off +
-                     comlang_ethno + colony + comcol + col45 + smctry +
-                     log( GDP_PP_d ) + log( GDP_PP_o ) + log( pop_d ) + log( pop_o ) +
-                     log( CL_o ) + log( CL_d ) + log( PR_o ) + log( PR_d ) +
-                     asinh( CPI_d ) + asinh( CPI_o ) + dead_d + dead_log_d +
-                     dead_o + dead_log_o + Nyear_conf_d + Nyear_log_d + Nyear_conf_o +
-                     Nyear_log_o |  iso_o + iso_d + year )
+form <- formula(  newarrival ~   
+                  index0asylum + log( dist ) +  contig + comlang_off +
+                  comlang_ethno + colony + comcol + col45 + smctry +
+                  log( GDP_PP_d ) + log( GDP_PP_o ) + log( pop_d ) + log( pop_o ) +
+                  log( CL_o ) + log( CL_d ) + log( PR_o ) + log( PR_d ) +
+                  asinh( CPI_d ) + asinh( CPI_o ) + dead_d + dead_log_d +
+                  dead_o + dead_log_o + Nyear_conf_d + Nyear_log_d + Nyear_conf_o +
+                  Nyear_log_o |  iso_o + iso_d + year )
 
 ### loop through five imputed data sets 
 for( i in 1:5 ){
@@ -41,7 +42,7 @@ for( i in 1:5 ){
    assign( paste0( "fennel1_", i ), femlm( form, data = impu17[[i]], family = "poisson" ))
 
    # negative binomial
-   assign( paste0( "fennel2_", i ), femlm( form, data = impu17[[i]], family = "negbin" ))
+   #assign( paste0( "fennel2_", i ), femlm( form, data = impu17[[i]], family = "negbin" ))
 }
 
 #### save results 
