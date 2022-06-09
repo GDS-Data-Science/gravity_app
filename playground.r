e6b2c95b4cad3467a9376e93578275921e37c1eb
geraldine28 <75378350+geraldine28@users.noreply.github.com>
2022-06-09 17:56:29 +0200

    Create playground.r
    Playground for gravity model.

---
+++ playground.r
@@ -0,0 +1,177 @@
+################################################################################
+################################################################################
+##                                                                            ##
+##                       Playground for Gravity Model                         ##
+##                                                                            ##
+################################################################################
+################################################################################
+
+
+#### load packages
+library( dplyr )
+library( fastDummies )
+library( ggplot2 )
+library( glmnet )
+library( hexbin )
+library( Metrics )
+library( purrr )
+library( readr )
+library( tidyr )
+
+#### read in data
+load( "../Data/WorkData/impuData17.Rdata" )
+dat <- impu17[[1]]
+
+################################################################################
+#                                    EDA                                       #
+################################################################################
+
+# structure of the data 
+str( dat )
+# summary of the variables
+summary( dat )
+## structure of the data
+# length of time series (balanced panel)
+table( table( dat$Id ))
+# number of unique country of origin 
+n_o <- length( unique( dat$iso_o ))
+# number of unique country of asylum
+n_d <- length( unique( dat$iso_d ))
+# number of unique country pairs 
+n_p <- length( unique( dat$Id ))
+# equality of countries in 'o' and 'd' category
+mean( unique( dat$iso_o ) %in% unique( dat$iso_d ))
+
+## visualization 
+# distribution of dependent variable 
+hist( dat$newarrival, main = "Distribution of 'newarrival' " )
+# share of zeros
+mean( dat$newarrival == 0 )
+# plot time series 
+dat %>% group_by( iso_o, year ) %>% 
+        summarise( total = sum( newarrival )) %>% 
+        filter( total > 100 ) %>% 
+        ggplot( aes( x = year, y = total, color = iso_o )) +
+        geom_line( )
+# impact of PR and CL on newarrival
+ggplot( data = dat, mapping = aes( x = log( newarrival ), 
+                                   y = ..density..,
+                                   colour = as.factor( PR_o ))) +
+   geom_freqpoly(  ) +
+   ggtitle( "Impact of political rights on the distribution of newarrival" )
+   
+ggplot( data = dat, mapping = aes( x = log( newarrival ),
+                                   y = ..density..,
+                                   colour = as.factor( CL_o ))) +
+   geom_freqpoly(  ) +
+   ggtitle( "Impact of civil liberty on the distribution of newarrival" )
+# impact of violence type on newarrival
+ggplot( data = dat, mapping = aes( x = log( newarrival ), 
+                                   y = ..density..,
+                                   colour = as.factor( typeOfViolence_o ))) +
+   geom_freqpoly(  ) +
+   ggtitle( "Impact of violence type on the distribution of newarrival" )
+# impact of island status 
+ggplot( data = dat, mapping = aes( x = as.factor( island_o ), y = log( newarrival ))) +
+   geom_boxplot()
+ggplot( data = dat, mapping = aes( x = as.factor( island_d ), y = log( newarrival ))) +
+   geom_boxplot()
+# impact of landlocked status 
+ggplot( data = dat, mapping = aes( x = as.factor( landlocked_o ), y = log( newarrival ))) +
+   geom_boxplot()
+# impact of prima facie 
+ggplot( data = dat, mapping = aes( x = as.factor( index0asylum ), y = log( newarrival ))) +
+   geom_boxplot()
+# correlation between GDP_PP and newarrival
+ggplot( data = dat ) +
+   geom_hex( mapping = aes( x = GDP_PP_o, y = log( newarrival )))
+ggplot( data = dat ) +
+   geom_hex( mapping = aes( x = GDP_PP_d, y = log( newarrival )))
+# correlation between best_est_o and newarrival
+ggplot( data = dat ) +
+   geom_hex( mapping = aes( x = best_est_o, y = newarrival ))
+# correlation between distance and newarrival
+ggplot( data = dat, mapping = aes( x = dist, y = log( newarrival ))) + 
+   geom_boxplot( mapping = aes( group = cut_number( dist, 10 )))
+
+
+################################################################################
+#                              Data Preparation                                #
+################################################################################
+
+### year wise cross validation 
+set.seed( 42 )
+# create cv matrix
+v <- t( replicate( 10, sample( 2000:2021, 2 )))
+
+# select variables from dat
+dat <- select( dat, -c( "Country_o", "Country_d", "Id" ))
+dat <- filter( dat, iso_o %in% c( "AFG", "ALB" ) & 
+                    iso_d %in% c( "DNK", "DJI", "DMA", "DOM", "ECU" ))
+# turn variables into factors 
+cols <- c( "iso_d", "PR_o", "CL_o", "typeOfViolence_o", 
+           "PR_d", "CL_d", "typeOfViolence_d" )
+# turn factors into dummy variables
+dat <- dummy_cols( dat, select_columns = cols, 
+                   remove_first_dummy = TRUE, remove_selected_columns = TRUE )
+
+
+################################################################################
+#                                LASSO model                                   #
+################################################################################
+
+mse_round <- rep( NA, 10 )
+
+## run cross validation glm.net
+for( i in 1:10 ){
+   
+   # nest data frame by country of origin
+   dat_nest <- dat %>% 
+               group_by( iso_o ) %>% 
+               nest()
+   
+   # generate training and testing data 
+   dat_tt <- dat_nest %>% 
+             mutate( train = map( data, ~ filter( .x, !( year %in% v[ i, ] ))), 
+                     test = map( data, ~filter( .x, year %in% v[ i, ]))) %>% 
+             mutate( train_x = map( train, ~ select( .x, -c( newarrival ))), 
+                     train_y = map( train, ~ select( .x, newarrival )), 
+                     test_x = map( test, ~ select( .x, -c( newarrival ))), 
+                     test_y = map( test, ~ select( .x, newarrival )))
+   
+   # estimate LASSO with training data 
+   fit <- dat_tt %>% 
+          mutate( model = map2( .x = train_x, .y = train_y,
+                                ~ cv.glmnet( as.matrix( .x ), as.matrix( .y ),
+                                             family = "poisson" )))
+   
+   # predict values for test data 
+   dat_p <- fit %>% 
+            mutate( p = map2( .x = model, .y = test_x, 
+                              ~ predict( .x, newx = as.matrix( .y ), 
+                                         type = "response", s = .x$lambda.min ))) 
+   
+   # calculate MSE 
+   dat_final <- dat_p %>% 
+                mutate( test_y = map( test_y, ~ as.numeric( .x$newarrival )),
+                        mse = map2( .x = test_y, .y = p, 
+                                    ~ rmse( as.numeric( .x ), .y ))) %>% 
+                unnest( mse )
+   
+   mse_round[i] <- mean( dat_final$mse )
+}
+
+
+mse_final <- mean( mse_round )
+
+
+
+
+
+
+
+
+
+
+
+
