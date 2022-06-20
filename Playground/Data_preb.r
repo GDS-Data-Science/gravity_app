1ec5e9315850924bb178f7ed4987ab271b19beab
geraldine28 <75378350+geraldine28@users.noreply.github.com>
2022-06-20 14:38:12 +0200

    Update Data_preb.r
    Finished coding time slices.

--- Playground/Data_preb.r
+++ Playground/Data_preb.r
@@ -13,9 +13,6 @@ library( purrr )
 library( readr )
 library( tidyr )
 
-
-
-
 #### read in data
 load( "../Data/WorkData/impuData17.Rdata" )
 dat <- impu17[[1]]
@@ -23,6 +20,31 @@ dat <- impu17[[1]]
 ################################################################################
 #                              Data Preparation                                #
 ################################################################################
+
+### year wise cross validation 
+set.seed( 42 )
+## create timeslice indices for CV with panel data
+# block length
+t <- length( unique( dat$year ))
+# number of cross-sectional observations
+n <- length( unique( dat$Id ))
+# length of window and horizon
+window_length <- 12
+horizon_length <- 1
+# add-on for block
+add_train <- sort( rep( seq( 0, n, by = t ), window_length ))
+add_test <- sort( rep( seq( 0, n, by = t ), horizon_length ))
+# generate y
+y <- 1:t
+# create timeslice object
+time_slice <- createTimeSlices( y, initialWindow = window_length, horizon = horizon_length )
+# length of list
+list_length <- length( time_slice$train )
+# adjust for rest of the blocks
+for( i in 1:list_length ){
+   time_slice$train[[i]] <- rep( time_slice$train[[i]], n ) + add_train
+   time_slice$test[[i]] <- rep( time_slice$test[[i]], n ) + add_test
+}
 
 ### select variables from dat
 dat <- select( dat, -c( "Country_o", "Country_d", "Id" ))
@@ -34,9 +56,6 @@ cols <- c( "iso_o", "iso_d", "year", "PR_o", "CL_o", "typeOfViolence_o",
            "comlang_off", "comlang_ethno", "colony", "comcol", "col45", "smctry" )
 dat[ cols ] <- lapply( dat[ cols ], factor )
 
-
-### year wise cross validation 
-set.seed( 42 )
 
 
 
