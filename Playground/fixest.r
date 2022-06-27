1772ad1a36911f9e9df2dcff74684e4b68659e6f
geraldine28 <75378350+geraldine28@users.noreply.github.com>
2022-06-27 08:48:21 +0200

    Cleaned up code estimation

--- Playground/fixest.r
+++ Playground/fixest.r
@@ -11,6 +11,7 @@ library( fixest )
 
 #### read in data 
 load( "../Data/WorkData/impuData17.Rdata" )
+dat <- impu17[[1]] 
 
 #### run estimations
 # estimation equation 
@@ -23,16 +24,12 @@ form <- formula(  newarrival ~
                   dead_o + dead_log_o + Nyear_conf_d + Nyear_log_d + Nyear_conf_o +
                   Nyear_log_o |  iso_o + iso_d + year )
 
-### loop through five imputed data sets 
-for( i in 1:5 ){
-   ## FENmlm
-   # poisson
-   assign( paste0( "fennel1_", i ), femlm( form, data = impu17[[i]], family = "poisson" ))
-}
+## FENmlm
+# poisson
+fennel1_1 <- femlm( form, data = impu17[[i]], family = "poisson" )
 
-#### save results 
-est_models_poisson <- list( fennel1_1, fennel1_2, fennel1_3, fennel1_4, fennel1_5 )
-save(  est_models_poisson, file = "../Results/estimations.Rdata" )
+# zero inflated poisson 
+
 
 
 
